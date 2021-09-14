391b397225e7696ce7a024df7b98d908e32ae500
Emperor D. Pengin <emperord.pengin@gmail.com>
2021-09-13 22:31:50 -0400

    MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 
 export default function MovieList(props) {
+  const { movies } = props;
+  
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -13,15 +18,22 @@ export default function MovieList(props) {
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
+  const linkStyle = {
+    color: 'black',
+    textDecoration: 'none' 
+  }
+
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+    <Link to={`/movies/${props.movie.id}`} style={linkStyle}>
+      <div className="movie-card" >
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    </Link>
   );
 }
